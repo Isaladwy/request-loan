f2ef03ed2805d7a82ede7af94f8baa07ce1ecef0
Isaladwy <Isaladwy@gmail.com>
2025-04-29 21:33:39 +0300

    style(LoanForm): enhance layout and styling for form elements

--- src/components/LoanForm.jsx
+++ src/components/LoanForm.jsx
@@ -1,14 +1,83 @@
 function LoanForm() {
   return (
-    <div>
+    <div style={{ backgroundColor: 'black', textAlign: 'center', width: '50%' }}>
       <form>
         <h1>Requesting a Loan</h1>
         <hr />
-        <label htmlFor="">Name</label>
-        <input type="text" />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <label htmlFor="">Phone</label>
+          <input type="tel" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <label htmlFor="">Age</label>
+          <input type="number" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <label htmlFor="">Are you an employee?</label>
+          <input type="checkbox" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: '20px',
+          }}
+        >
+          <label htmlFor="">Salary</label>
+          <select>
+            <option>less than $500</option>
+            <option>between $500 and $2000</option>
+            <option>above $2000</option>
+          </select>
+        </div>
+        <button >Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default LoanForm
+export default LoanForm;
